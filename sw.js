0fc7b3ad157d56fecd85032c78156ed592ad55d1
DaliaG-09 <golacg09@gmail.com>
2026-08-23 13:53:37 -0500

    Bump version cache

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v44';
+const CACHE_NAME = 'medcore-v45';
 const ASSETS = [
   './',
   './index.html',
