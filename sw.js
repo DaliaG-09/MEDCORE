906199c6e8e7510eead4042ff61af19975b034a9
DaliaG-09 <golacg09@gmail.com>
2026-08-23 22:30:58 -0500

    Paridad de opciones en sesiones de hospital: cuaderno de clase + fotos generales por día (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v55';
+const CACHE_NAME = 'medcore-v56';
 const ASSETS = [
   './',
   './index.html',
