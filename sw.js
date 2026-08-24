cba3885dfdf1268c4130b123d415d37b3c93d136
DaliaG-09 <golacg09@gmail.com>
2026-08-24 00:10:39 -0500

    Cuaderno: imágenes pegadas/subidas directo en el lienzo + versión incrustada sticky en sesiones de hospital (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v56';
+const CACHE_NAME = 'medcore-v57';
 const ASSETS = [
   './',
   './index.html',
