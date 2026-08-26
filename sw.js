e747651ca56e02c601e5f6c787859736c0e251aa
DaliaG-09 <golacg09@gmail.com>
2026-08-25 23:56:50 -0500

    Reestructura Talleres como secciones propias e independientes, no mezcladas en enfermedades (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v65';
+const CACHE_NAME = 'medcore-v66';
 const ASSETS = [
   './',
   './index.html',
