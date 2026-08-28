725417bf326b2d707ee0d50497ac4bf3ab3e6817
DaliaG-09 <golacg09@gmail.com>
2026-08-28 01:07:37 -0500

    Arregla: tildes/puntos ya no se borran solos + doble cronómetro al abandonar un examen (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v77';
+const CACHE_NAME = 'medcore-v78';
 const ASSETS = [
   './',
   './index.html',
