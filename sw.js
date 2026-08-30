5ebd8f5b2f16acd41316c58b72490b8287849475
DaliaG-09 <golacg09@gmail.com>
2026-08-29 20:18:32 -0500

    Bronquitis/Bronquiectasias reconstruida con estándar EPOC: imagen TC real, tablas comparativas, caso real del PPT (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v80';
+const CACHE_NAME = 'medcore-v81';
 const ASSETS = [
   './',
   './index.html',
@@ -30,6 +30,7 @@ const ASSETS = [
   './assets/epoc/epoc-patogenia-diagrama.jpg',
   './assets/asma/asma-cascada-th2-ilc2.jpg',
   './assets/asma/asma-mastocitos-basofilos.jpg',
+  './assets/bronquitis/bronquiectasias-tc-patrones.jpg',
   './assets/epoc/epoc-rx-hiperinflacion.jpg',
   './assets/nexu-logo.svg',
   './assets/referencia-panalizacion-tc.png',
