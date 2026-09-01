00f4c443e71d23d80820b6486625a46fcc8a566a
DaliaG-09 <golacg09@gmail.com>
2026-08-31 20:29:09 -0500

    Los 4 talleres completados: PDF real enlazado + examen simulado + vista dividida (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v92';
+const CACHE_NAME = 'medcore-v93';
 const ASSETS = [
   './',
   './index.html',
