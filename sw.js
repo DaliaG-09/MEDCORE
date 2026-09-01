f51290812c64dd0d5345d75a77e089e1d07c2ca1
DaliaG-09 <golacg09@gmail.com>
2026-08-31 20:07:57 -0500

    Exámenes de Ayuda Diagnóstica completado: PDF enlazado, tablas de patrones reales, caso real EPOC (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v90';
+const CACHE_NAME = 'medcore-v91';
 const ASSETS = [
   './',
   './index.html',
