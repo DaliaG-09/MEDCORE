f2c70d38a13769f4ab93409c00e71c2c271ad16e
DaliaG-09 <golacg09@gmail.com>
2026-09-01 16:33:16 -0500

    Rediseño de 'Mis Notas': barra de progreso segmentada por peso real, colores/iconos por módulo (reusando los de la app), tarjetas de examen diferenciadas de las de EC (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v100';
+const CACHE_NAME = 'medcore-v101';
 const ASSETS = [
   './',
   './index.html',
