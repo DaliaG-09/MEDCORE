5e6fc604f6e6c8760064b5bda4d6e6a67aaf47d3
DaliaG-09 <golacg09@gmail.com>
2026-08-31 21:17:41 -0500

    Imagen del caso de neumotórax agregada también a Modo Profundo + confirmada coherencia de los 3 modos en las 11 enfermedades (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v95';
+const CACHE_NAME = 'medcore-v96';
 const ASSETS = [
   './',
   './index.html',
