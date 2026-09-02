a6b53cfe4e7868971d1af13fb3d76e3c88d0f84b
DaliaG-09 <golacg09@gmail.com>
2026-09-02 12:07:43 -0500

    Semanas 1-2 completadas contra PPT real: 6 datos menores agregados en Asma, Bronquitis, NAC, NIH, EPOC (+ tabla de 4 endotipos) y Pleurales (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v103';
+const CACHE_NAME = 'medcore-v104';
 const ASSETS = [
   './',
   './index.html',
