dcafb620db9ec487c34745e5e53348b66a4f675b
DaliaG-09 <golacg09@gmail.com>
2026-09-07 14:58:41 -0500

    Módulo Cardiología creado formalmente (con sus 2 enfermedades + examen final + 3 casos comparativos), y botón agregado al menú (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v114';
+const CACHE_NAME = 'medcore-v115';
 const ASSETS = [
   './',
   './index.html',
