8c57dd1aae458f1798b9ba4dca75239a4fdcbced
DaliaG-09 <golacg09@gmail.com>
2026-09-10 17:59:00 -0500

    Sexta auditoría de Semana 5: lineas de tratamiento escalonado + reduccion de corticoides (Pericardio), control de frecuencia + extrasistoles ventriculares (Arritmias) (sw.js)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_NAME (ej. 'medcore-v2') y el navegador refresca la copia.
    ============================================================ */
 
-const CACHE_NAME = 'medcore-v127';
+const CACHE_NAME = 'medcore-v128';
 const ASSETS = [
   './',
   './index.html',
@@ -63,6 +63,7 @@ const ASSETS = [
   './assets/cardio/arritmia-fa-practica-ekg.jpg',
   './assets/cardio/arritmia-flutter-practica-ekg.jpg',
   './assets/cardio/arritmia-wpw-ecg-comparacion.jpg',
+  './assets/cardio/arritmia-extrasistoles-ventriculares.jpg',
   './assets/cardio/arritmia-fibrilacion-ventricular.jpg',
   './assets/cardio/conduccion-brd-ekg.jpg',
   './assets/cardio/conduccion-bri-ekg.jpg',
